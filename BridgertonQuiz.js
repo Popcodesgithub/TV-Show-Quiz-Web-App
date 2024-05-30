3e8db88036a8dc5664e21920ccdd3cdaeb8fc329
Pop <popcodesgithub@gmail.com>
2024-05-30 14:54:15 +0200

    added comments to JS

--- BridgertonQuiz.js
+++ BridgertonQuiz.js
@@ -1,10 +1,13 @@
+//Defining elements 
 const startButton = document.getElementById('startBtn')
 const nextButton = document.getElementById('nextBtn')
 const questionContainerElement = document.getElementById('questionContainer')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answerBtn')
 
-let shuffledQuestions, currentQuestionIndex
+//These are 'let' instead of 'const' because they will need to be updated as we go through the quiz.
+let shuffledQuestions 
+let currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
@@ -12,6 +15,7 @@ nextButton.addEventListener('click', () => {
   setNextQuestion()
 })
 
+//function that hides start button, randomises question selection and unhides question container.
 function startGame() {
   startButton.classList.add('hide')
   shuffledQuestions = questions.sort(() => Math.random() - .5)
@@ -20,11 +24,13 @@ function startGame() {
   setNextQuestion()
 }
 
+//Function to call on 2 other functions that on next question clears and laods new question.
 function setNextQuestion() {
   resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+// creates a button that reveals the correct answer on the eventlistener click
 function showQuestion(question) {
   questionElement.innerText = question.question
   question.answers.forEach(answer => {
@@ -39,6 +45,7 @@ function showQuestion(question) {
   })
 }
 
+// this is a function to reset everything (new q and color changes) when the next button is selected
 function resetState() {
   clearStatusClass(document.body)
   nextButton.classList.add('hide')
@@ -47,6 +54,7 @@ function resetState() {
   }
 }
 
+//shuffles questions 
 function selectAnswer(e) {
   const selectedButton = e.target
   const correct = selectedButton.dataset.correct
@@ -60,12 +68,14 @@ function selectAnswer(e) {
     startButton.innerText = 'Restart'
     startButton.classList.remove('hide')
 
+// Prompts for participants name and alerts them with a fun msg.
     let name = prompt("Please enter your name", "John Smith");
     console.log(name)
     let alertMsg = alert("Well done! " + name);
 }
 }
 
+// this is a function to reset everything 
 function setStatusClass(element, correct) {
   clearStatusClass(element)
   if (correct) {
@@ -75,11 +85,14 @@ function setStatusClass(element, correct) {
   }
 }
 
+//To clear and reset the quiz, used also above
 function clearStatusClass(element) {
   element.classList.remove('correct')
   element.classList.remove('wrong')
 }
 
+
+//All the quiz questions including the answers
 const questions = [
   {
     question: 'On which book, is the first season of Bridgerton based?',
